d2343940265438d9bc42742a6e8ba32a17b13477
Sinka <xgao@zoyoe.com>
2023-09-16 15:35:43 +0800

    partial fix test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
-rm -rf output
-rm -rf params
+#rm -rf output
+#rm -rf params
 
-mkdir output
-mkdir params
+#mkdir output
+#mkdir params
 # Get the resource ready for tests
 cargo test --release
 
 # verify generated proof for test circuits
-cargo run --release --features cuda -- --param ./params --output ./output verify --info output/test1.loadinfo.json
+cargo run --release --features cuda -- --param ./params --output ./output verify --challenge poseidon --info output/test1.loadinfo.json
 
 # batch test proofs
-cargo run --release --features cuda -- --param ./params --output ./output batch --challenge sha -k 21 --info output/test2.loadinfo.json output/test1.loadinfo.json --name batchsample --commits sample/batchinfo1.json
+cargo run --release --features cuda -- --param ./params --output ./output batch -k 22 --challenge sha --info output/test2.loadinfo.json output/test1.loadinfo.json --name batchsample --commits sample/batchinfo1.json
 
 
 # verify generated proof for test circuits
