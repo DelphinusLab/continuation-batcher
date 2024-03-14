13284511d39f4d8fe20a054f3848a28d2d81ad03
rhaoio <richard@rhao.io>
2024-03-14 17:26:58 +1100

    update keccak test script

--- test_keccak.sh
+++ test_keccak.sh
@@ -1,17 +1,21 @@
 rm -rf output
 rm -rf params
 
+rm -f ./sol/contracts/AggregatorConfig.sol
+rm -f ./sol/contracts/AggregatorVerifierStep1.sol
+rm -f ./sol/contracts/AggregatorVerifierStep2.sol
+rm -f ./sol/contracts/AggregatorVerifierStep3.sol
+
 mkdir output
 mkdir params
 # Get the resource ready for tests
 cargo test --release
 
 # verify generated proof for test circuits
-cargo run --release --features cuda -- --param ./params --output ./output verify --challenge poseidon --info output/test1.loadinfo.json
+cargo run --release --features cuda -- --param ./params --output ./output verify --challenge poseidon --info output/test_circuit.loadinfo.json
 
 # batch test proofs
-cargo run --release --features cuda -- --param ./params --output ./output batch -k 22 --challenge keccak --info output/test2.loadinfo.json output/test1.loadinfo.json --name batchsample --commits sample/batchinfo1.json
-
+cargo run --release --features cuda -- --param ./params --output ./output batch -k 22 --challenge keccak --info output/test_circuit.loadinfo.json --name batchsample --commits sample/batchinfo1.json
 
 # verify generated proof for test circuits
 cargo run --release --features cuda -- --param ./params --output ./output verify --challenge keccak --info output/batchsample.loadinfo.json
