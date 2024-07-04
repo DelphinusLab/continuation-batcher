ce041db2f376fef12d9333a597ab26c98def18a3
sinka <xgao@zoyoe.com>
2024-07-04 12:52:00 +1000

    polish test_cont.sh

--- scripts/test_cont.sh
+++ scripts/test_cont.sh
@@ -1,3 +1,18 @@
+CONFIGDIR="."
+## circuit batcher
+# BATCHER_DIR=$WKDIR/continuation-batcher
+# BATCHER=$BATCHER_DIR/target/release/circuit-batcher
 
-cargo run --release --features cuda -- --params ./params --output ./output batch -k 22 --challenge sha --info output/wasm_output.loadinfo.json --name wasm_output_agg --commits sample/batchinfo_cont.json --cont
-cargo run --release --features cuda -- --params ./params --output ./output verify --challenge sha --info output/wasm_output_agg.loadinfo.json
+BATCH_INFO_INIT=$CONFIGDIR/sample/cont-init.json
+BATCH_INFO_RECT=$CONFIGDIR/sample/cont-rec.json
+BATCH_INFO_FINAL=$CONFIGDIR/sample/cont-final.json
+
+WKDIR="/home/xgao/zkWasm"
+
+# Make sure the name field in fibonacci.loadinfo.json is changed to single to fit the above batch configure
+
+# gen solidity failed, but proof generation succeeds
+# k must be 23, otherwise it'll throws not enough rows at the last final round (if you are not care about the final round to be is_final, set k = 22)
+RUST_BACKTRACE=1 cargo run --features perf --release -- --params $WKDIR/params --output $WKDIR/output batch -k 22  -s shplonk --challenge sha --info $WKDIR/output/fibonacci.loadinfo.json --name fib_agg --commits $BATCH_INFO_INIT $BATCH_INFO_RECT $BATCH_INFO_FINAL --cont
+
+cargo run --release --features cuda -- --params $WKDIR/params --output $WKDIR/output verify --challenge sha --info output/fib_agg.loadinfo.json
